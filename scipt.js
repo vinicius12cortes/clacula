108c2858144ec914dbf3c0deb1cf04c7e091b3d6
vinicius12cortes <137785620+vinicius12cortes@users.noreply.github.com>
2023-09-19 11:47:23 -0300

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,21 @@
+calcule.addEventListener("click",function(){
+var a  = valorA.value; 
+var b  = valorB.value;
+//var op =  opecao.value;
+
+
+a  =  parseInt(a);
+b  =  parseInt(b);
+
+
+
+var result = a+b
+
+
+
+alert(result);
+
+
+ 
+
+});
